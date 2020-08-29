39b79135c891600dd8f77b259c2447e4ed82e11a
shaharma <32978857+shaharma@users.noreply.github.com>
2020-08-30 01:39:46 +0300

    updated keypress to keydown
    updated keypress to keydown

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   });
 }
 
-document.addEventListener("keypress", function (event) {
+document.addEventListener("keydown", function (event) {
   makeSound(event.key);
 
   buttonAnimation(event.key);
